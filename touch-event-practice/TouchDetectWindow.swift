d17daf77f2ef681ae193ba6af6696c1c71d82342
Shima <shima.jin@icloud.com>
2020-09-28 02:26:13 +0900

    comment

--- touch-event-practice/TouchDetectWindow.swift
+++ touch-event-practice/TouchDetectWindow.swift
@@ -20,12 +20,15 @@ final class TouchDetectWindow: UIWindow {
   }
 
   override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
+//    return nil
     let view = super.hitTest(point, with: event)
     if let view = view {
-      print("hittest window", view)
+      print("hittest window", String(describing: type(of: view)))
     } else {
       print("hittest window nil")
     }
+    // TODO: ここでTooltipのタッチ判定を行ってTooltipをタッチしていたらnilを返すようにしたらTooltipの後ろのタップが発火しないかも
+
     return view
   }
 
